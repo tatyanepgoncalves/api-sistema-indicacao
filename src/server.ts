1afb1f8058b9c1de1a7c7043d3f3aa5b2c66e30b
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-04-02 20:35:30 -0300

    :sparkles: feat: Create simple route POST Subscriptions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,47 @@
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.get("/hello", () => {
-  return "Hello Taty!"
-})
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
+
+app.post(
+  "/subscriptions",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.string(),
+        }),
+      }
+    },
+  },
+  async (request, reply) => {
+    const { name, email } = request.body;
+
+    return reply.status(201).send({ 
+      name, 
+      email 
+    });
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("🚀 HTTP server is running!")
-})
+  console.log("🚀 HTTP server is running!");
+});
