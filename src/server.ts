8c401a288bc2f724c49d8ff1d8f1dad971690ed9
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-04-02 21:16:45 -0300

    :sparkles: feat: Configuration of development variables

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { createSubscriptionsRoute } from "./routes/createSubscriptionsRoute"
+import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -37,6 +38,6 @@ app.register(fastifySwaggerUi, {
 app.register(createSubscriptionsRoute)
 
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("🚀 HTTP server is running!");
 });
